f97a54fd3d4fec3c2e879301ae619840a8d84075
Ben Shepherd <b.c.shepherd@hotmail.com>
2025-01-04 12:43:31 +0000

    Fixed unit test and configured to run headless

--- client/src/app/landing-page/landing-page.component.spec.ts
+++ client/src/app/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandingPageComponent } from './landing-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -8,7 +9,7 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingPageComponent]
+      imports: [LandingPageComponent, BrowserAnimationsModule]
     })
     .compileComponents();
 
