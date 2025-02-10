5f3c16c6ec360e990845988492334e05c9cc1897
Ben Shepherd <b.c.shepherd@hotmail.com>
2025-02-10 21:30:55 +0000

    Fixed start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Start the .NET API in the background
 echo "Starting .NET API..."
-dotnet /app/publish/PersonalWebsite.Api.dll &
+dotnet /app/publish/PersonalWebsiteBFF.Api.dll &
 
 # Start Nginx in the foreground
 echo "Starting Nginx..."
