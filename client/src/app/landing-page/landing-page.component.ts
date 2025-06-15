11d4b757a9d9c3eb1035d648b0a1266b0bc94996
Ben Shepherd <b.c.shepherd@hotmail.com>
2025-06-15 11:05:51 +0100

    Added demo applications tab

--- client/src/app/landing-page/landing-page.component.ts
+++ client/src/app/landing-page/landing-page.component.ts
@@ -4,13 +4,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTabsModule } from '@angular/material/tabs';
 import { SideProjectsComponent } from '../side-projects/side-projects.component';
 import { CurriculumVitaeComponent } from '../curriculum-vitae/curriculum-vitae.component';
+import { DemoAppsComponent } from '../demo-apps/demo-apps.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [MatCardModule, MatButtonModule, SideProjectsComponent, MatTabsModule, CurriculumVitaeComponent],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    SideProjectsComponent,
+    MatTabsModule,
+    CurriculumVitaeComponent,
+    DemoAppsComponent,
+  ],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
-
-}
+export class LandingPageComponent {}
