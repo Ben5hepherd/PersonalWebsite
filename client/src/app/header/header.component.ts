3d8b36e03acc70628836117cfb535d396ad47014
Ben Shepherd <b.c.shepherd@hotmail.com>
2025-08-23 14:44:54 +0100

    Switched home and login buttons for relevant icons

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
